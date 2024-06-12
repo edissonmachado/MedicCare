f9a66d99b28386bd8e068d42c285557734ddcc6e
edissonmachado <edisson.machado@gmail.com>
2024-06-12 16:42:55 -0500

    Add patient and generic repository initial implementations

---
+++ MedicCare.Persistence/Common/Repository.cs
@@ -0,0 +1,12 @@
+﻿using MedicCare.App.Common;
+
+namespace MedicCare.Persistence.Common
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MedicCare.Persistence/Patients/PatientRepository.cs
@@ -0,0 +1,14 @@
+﻿using MedicCare.App.Patients;
+using MedicCare.Domain;
+using MedicCare.Persistence.Common;
+
+namespace MedicCare.Persistence.Patients
+{
+    public class PatientRepository : Repository<Patient>, IPatientRepository
+    {
+        public async Task<List<Encounter>> GetPatientReports()
+        {
+            return await Task.FromResult(new List<Encounter>()).ConfigureAwait(false);
+        }
+    }
+}
