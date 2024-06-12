749d61d3adc492f062207ff232f18e994c7aef28
edissonmachado <edisson.machado@gmail.com>
2024-06-12 16:42:04 -0500

    Add patient and generic repository interfaces

---
+++ MedicCare.App/Common/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using MedicCare.Domain;
+using System.Threading.Tasks;
+
+namespace MedicCare.App.Common
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+    }
+}
---
+++ MedicCare.App/Patients/IPatientRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MedicCare.App.Common;
+using MedicCare.Domain;
+
+namespace MedicCare.App.Patients
+{
+    public interface IPatientRepository : IRepository<Patient>
+    {
+        Task<List<Encounter>> GetPatientReports();
+    }
+}
