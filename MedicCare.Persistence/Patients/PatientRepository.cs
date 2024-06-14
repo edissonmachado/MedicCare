0fba40c56afea584ee0296315e255c1c0748c8ac
edissonmachado <edisson.machado@gmail.com>
2024-06-14 17:20:56 -0500

    Fix sql query formatting and variable naming

--- MedicCare.Persistence/Patients/PatientRepository.cs
+++ MedicCare.Persistence/Patients/PatientRepository.cs
@@ -16,37 +16,41 @@ namespace MedicCare.Persistence.Patients
 
         public async Task<List<Encounter>> GetEncounters()
         {
-            string query = "WITH tempEncounters as " +
-                    " (SELECT " +
-                    "	encounter.* " +
-                    "	 , ROW_NUMBER() OVER(partition by pa.Id, py.Id order by pa.Id, py.Id) AS ROWNO " +
-                    "    , pa.Id " +
-                    "    , pa.firstname as firstName " +
-                    "    , pa.lastname as lastName " +
-                    "    , pa.age " +
-                    "    , py.Id " +
-                    "    , py.payer_name as CompanyName " +
-                    "    , py.city as CompanyCity " +
-                    "    , f.Id " +
-                    "    , f.facility_name as BranchName " +
-                    "    , f.city as BranchCity " +
-                    " FROM encounter " +
-                    " INNER JOIN payer as py on encounter.payer_id = py.Id " +
-                    " INNER JOIN patient as pa on encounter.patient_id = pa.Id " +
-                    " INNER JOIN facility as f on encounter.facility_id = f.Id)" +
-                " SELECT * FROM tempEncounters c1 " +
-                "    JOIN (SELECT patient_id, payer_id, MAX(rowno) as maxVal FROM tempEncounters group by patient_id, payer_id) c2 " +
-                "    ON c1.patient_id=c2.patient_id AND c1.payer_id=c2.payer_id AND c1.rowno=c2.maxVal " +
-                "    ORDER BY rowno;";
+            string query =
+                @"WITH tempEncounters as (
+                     SELECT encounter.* 
+                    	, ROW_NUMBER() OVER(partition by pa.Id, py.Id order by pa.Id, py.Id) AS ROWNO 
+                        , pa.Id 
+                        , pa.firstname as firstName 
+                        , pa.lastname as lastName 
+                        , pa.age 
+                        , py.Id 
+                        , py.payer_name as CompanyName 
+                        , py.city as CompanyCity 
+                        , f.Id 
+                        , f.facility_name as BranchName 
+                        , f.city as BranchCity 
+                     FROM encounter 
+                     INNER JOIN payer as py on encounter.payer_id = py.Id 
+                     INNER JOIN patient as pa on encounter.patient_id = pa.Id 
+                     INNER JOIN facility as f on encounter.facility_id = f.Id
+                )
+                 
+                SELECT * FROM tempEncounters AS results 
+                JOIN (SELECT patient_id, payer_id, MAX(rowno) as maxVal 
+                       FROM tempEncounters group by patient_id, payer_id) AS values 
+                ON results.patient_id=values.patient_id
+                AND results.payer_id=values.payer_id AND results.rowno=values.maxVal 
+                ORDER BY rowno;";
 
             using var connection = _context.CreateConnection();
             var result = await connection.QueryAsync<Encounter, Patient, Payer, Facility, Encounter>(query,
-                (e, pa, py, f) =>
+                (encounter, patient, payer, facility) =>
                 {
-                    e.Patient = pa;
-                    e.Payer = py;
-                    e.Facility = f;
-                    return e;
+                    encounter.Patient = patient;
+                    encounter.Payer = payer;
+                    encounter.Facility = facility;
+                    return encounter;
                 }).ConfigureAwait(false);
 
             return result.ToList();
