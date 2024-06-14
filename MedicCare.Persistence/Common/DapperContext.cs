2f313bdba6923d12235703301e3dee0d7b7b693a
edissonmachado <edisson.machado@gmail.com>
2024-06-14 10:50:20 -0500

    Format code

--- MedicCare.Persistence/Common/DapperContext.cs
+++ MedicCare.Persistence/Common/DapperContext.cs
@@ -6,11 +6,12 @@ namespace MedicCare.Persistence.Common
     public class DapperContext : IDbContext
     {
         private readonly string _connectionString;
+
         public DapperContext(string connectionString)
         {
             _connectionString = connectionString;
         }
-        public IDbConnection CreateConnection()
-            => new NpgsqlConnection(_connectionString);
+
+        public IDbConnection CreateConnection() => new NpgsqlConnection(_connectionString);
     }
 }
